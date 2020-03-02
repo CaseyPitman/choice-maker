46df12495c1fa1e984006e4d1ccab804ec0d2c53
Casey The Great <casey@caseypitman.com>
2020-03-02 15:07:08 -0600

    basic functionality intact. Requires styles.

--- js/main.js
+++ js/main.js
@@ -15,25 +15,46 @@ $(document).ready(function(){
             $("#choices-input").append("<input id='option-"+i+"' class='input fields form-control' type='text' placeholder='Enter choice' val=''>"); 
     }
         $("#content-wrapper-intro").hide();
-        $("#content-wrapper-input").show();
+        $("#content-wrapper-input").fadeIn(1750);
       return false;     
     });
     
     //Array to store input choices
-        let choiceArr=[];
+       
 
     //Randomly choose from input options
     $("#make-choice").click(function(){
+        let choiceArr=[];
         //number of choices
         let optionCount=($("#option-count").val());
-        //iterate and push to choiceArr
-
+        //iterate choices and add to array
+        for (let i=0; i<optionCount;i++){
+            let id1="#option-";
+            let idNum=i;
+            let id=id1.concat(idNum);
+            let choice=$(id).val();
+            choiceArr.push(choice);
+        }
+        //select random number
+        let resultNum = Math.floor(Math.random()*optionCount);
+        //use random number to make choice from the option array
+        let result = choiceArr[resultNum];
+        
+        $("#result").text(result);
+        $("#result").hide("fast")
+        $("#content-wrapper-input").hide();
+        $("#result-content-wrapper").show();
+        $("#result").fadeIn(2000);
+        return false;
     });
   
+    $("#reload").click(function(){
+        location.reload();
+    })
 
 
 
-//}
+
 
 
 //call number of choices
