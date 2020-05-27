306b59f5a6aea461ce92364700fd7192646899d0
Casey The Great <casey@caseypitman.com>
2020-05-27 11:37:38 -0500

    Clean up

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-//Web app to make a decision
-$(document).ready(function(){
-
-//User inputs the number of options they have to choose from 
-//and will be given that number of input fields
+//User inputs the number of options they have to choose from and will be 
+//given that number of input fields.
 
     $("#input-option-count").click(function(){
-        //gets the number of desired options
+    //gets the number of desired options
      let optionCount= $("#option-count").val();
         //Loop the number of options and create that number of input fields
         for (let i=0; i<optionCount;i++){
             $("#choices-input").append("<input id='option-"+i+"' class='input-fields form-control' type='text' placeholder='Enter choice' val=''>"); 
-    }
+        }
+        //Hide opening screen
         $("#content-wrapper-intro").hide();
+        //Show choice entry screen
         $("#content-wrapper-input").fadeIn(1750);
       return false;     
     });
    
     //Randomly choose from input options
     $("#make-choice").click(function(){
-        //store choices
+        //store choices in an array
         let choiceArr=[];
         //number of choices
         let optionCount=($("#option-count").val());
@@ -30,11 +29,12 @@ $(document).ready(function(){
             let choice=$(id).val();
             choiceArr.push(choice);
         }
+
         //select random number
         let resultNum = Math.floor(Math.random()*optionCount);
         //use random number to make choice from the option array
         let result = choiceArr[resultNum];
-        
+        //Display the result
         $("#result").text(result);
         $("#result").hide("fast");
         $("#reload").hide("fast");
@@ -44,9 +44,8 @@ $(document).ready(function(){
         $("#reload").fadeIn(3000);
         return false;
     });
-  
+    //Clear and start over
     $("#reload").click(function(){
         location.reload();
     })
 
-});
